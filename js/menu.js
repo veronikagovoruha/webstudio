ee20d9c4c6c4c761de968336fc3fb1744fbe20af
Veronika <veronikagovoruha@gmail.com>
2022-04-01 15:04:11 +0300

    add transform to mobile menu

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,7 @@
 (() => {
     const menuBtnRef =document.querySelector("[data-menu-button]");
     const mobileMenuRef = document.querySelector("[data-menu]");
-    const logoRef = document.querySelector(".nav-logo");
+    // const logoRef = document.querySelector(".nav-logo");
 
     menuBtnRef.addEventListener("click", () =>{
         const expanded = 
@@ -11,7 +11,7 @@
         menuBtnRef.setAttribute("aria-expanded", !expanded);
 
         mobileMenuRef.classList.toggle("is-open");
-        logoRef.classList.toggle("hide-logo");
+        // logoRef.classList.toggle("hide-logo");
     });
 
 })();
